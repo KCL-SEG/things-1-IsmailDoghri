8efb08b0f691f8fd65f9f0839aefce975cc0b187
Ismail Doghri <ismail.doghri@kcl.ac.uk>
2022-10-19 14:25:06 +0100

    Add a basic things model5

--- things/models.py
+++ things/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 class Thing(models.Model):
     name = models.CharField(
+        max_length=30,
         unique=True,
         blank=False
     )
     description = models.TextField(
+        max_length=50,
         unique=False,
         blank=True
     )
